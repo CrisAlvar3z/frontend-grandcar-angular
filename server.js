41c61ff1c9e3d9bb68657c67b4a829cc85a71198
CrisAlvar3z <cris.alvarez.tobal@gmail.com>
2021-11-30 00:38:06 -0300

    cambios server.js

--- server.js
+++ server.js
@@ -1,32 +1,21 @@
-// Use Express
-var express = require("express");
-// Use body-parser
-var bodyParser = require("body-parser");
+const compression = require('compression');
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
 
-// Create new instance of the express server
-var app = express();
+// Gzip
+app.use(compression());
 
-// Define the JSON parser as a default way 
-// to consume and produce data through the 
-// exposed APIs
-app.use(bodyParser.json());
+// Run the app by serving the static files in the dist directory
+app.use(express.static(__dirname + '/dist/frontend-grandcar-angular'));
 
-// Create link to Angular build directory
-// The `ng build` command will save the result
-// under the `dist` folder.
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+// Start the app by listening on the default Heroku port
+app.listen(port);
 
-// Init the server
-var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
+// For all GET requests, send back index.html so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/frontend-grandcar-angular/index.html'));
 });
 
-/*  "/api/status"
- *   GET: Get server status
- *   PS: it's just an example, not mandatory
- */
-app.get("/api/status", function (req, res) {
-    res.status(200).json({ status: "UP" });
-});
+console.log(`Server listening on ${port}`);
