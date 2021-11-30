8220cb36592a7c1b4dc9231010eccc470bac6a88
CrisAlvar3z <cris.alvarez.tobal@gmail.com>
2021-11-30 01:11:31 -0300

    cambios en enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-grandcar.herokuapp.com/'
+  apiUrl: 'https://backend-grandcar.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://backend-grandcar.herokuapp.com/'
+  apiUrl: 'http://localhost:4000'
 };
 
 /*
