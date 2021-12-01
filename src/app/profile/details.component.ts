4731fa7ba6c1797c4450ebd686d9afdef15117b7
CrisAlvar3z <cris.alvarez.tobal@gmail.com>
2021-12-01 01:16:10 -0300

    cambios varios

--- src/app/profile/details.component.ts
+++ src/app/profile/details.component.ts
@@ -5,6 +5,7 @@ import { AccountService } from '@app/_services';
 @Component({ templateUrl: 'details.component.html' })
 export class DetailsComponent {
     account = this.accountService.accountValue;
+    direccion = this.account.domicilio['direccion'];
     constructor(private accountService: AccountService) {}
 }
 
